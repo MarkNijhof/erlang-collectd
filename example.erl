c18688fd15a30698278b92f1531e664d30d47689
Mathieu Lecarme <mathieu@garambrogne.net>
2011-05-24 23:23:47 +0200

    Use now default server.

--- example.erl
+++ example.erl
@@ -5,26 +5,29 @@
 
 start() ->
     application:start(collectd),
-    collectd:add_server(1),
     loop().
 
 loop() ->
-    collectd:set_gauge(ps_count, erlang,
-		       [erlang:system_info(process_count), erlang:system_info(schedulers_online)]),
+    collectd:set_gauge(ps_count, erlang,[
+        erlang:system_info(process_count),
+        erlang:system_info(schedulers_online)
+    ]),
     collectd:inc_counter(counter, inc, [1]),
     lists:foreach(fun({Type, Size}) ->
-			  collectd:set_gauge(memory, Type, [Size])
-		  end, erlang:memory()),
+        collectd:set_gauge(memory, Type, [Size])
+    end, erlang:memory()),
     {RunTime, _} = erlang:statistics(runtime),
     collectd:set_counter(cpu, runtime, [RunTime]),
     {Reductions, _} = erlang:statistics(reductions),
     collectd:set_counter(counter, reductions, [Reductions]),
-    collectd:set_gauge(queue_length, run_queue, [erlang:statistics(run_queue)]),
+    collectd:set_gauge(queue_length, run_queue, [
+        erlang:statistics(run_queue)
+    ]),
     sleep(1000),
     ?MODULE:loop().
 
 sleep(I) ->
     receive
-	after I ->
-		ok
-	end.
+        after I ->
+            ok
+    end.
