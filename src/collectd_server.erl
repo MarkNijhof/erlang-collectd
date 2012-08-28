716b4738795b9425bdfde15dde0efdb4063a42e1
Alex Arnell <alex@bravenet.com>
2012-08-28 11:31:15 -0700

    shrink max items even more

--- src/collectd_server.erl
+++ src/collectd_server.erl
@@ -9,7 +9,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	 terminate/2, code_change/3]).
 
--define(CHUNK_SIZE, 500).
+-define(CHUNK_SIZE, 30).
 -record(state, {sock, interval, host, port, values}).
 
 %%====================================================================
